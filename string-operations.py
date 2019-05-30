bc8471a7dab50c0bd6bf1ab660cb67ae0165ddbb
AndriTinas <andrianustinas.siskom@gmail.com>
2019-05-30 16:47:11 +0700

    string operation

---
+++ string-operations.py
@@ -0,0 +1,24 @@
+asstring='hallo cai'
+
+#print(len(asstring))
+
+#print(asstring.index('a'))
+
+#print(asstring.count('a'))
+
+#print(asstring[3:7])
+
+#print(asstring[3:7:2])
+
+#print(asstring[::-1])
+
+print(asstring.upper())
+
+print(asstring.lower())
+
+print(asstring.startswith('hallo'))
+
+print(asstring.endswith('hhddjfjhdh'))
+
+print(asstring.split(' '))
+
