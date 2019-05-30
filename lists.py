851e06d641e02b8ace785f907d237f264545f027
AndriTinas <andrianustinas.siskom@gmail.com>
2019-05-30 15:52:44 +0700

    list & operator

---
+++ lists.py
@@ -0,0 +1,20 @@
+witems = []
+
+items.append(1)
+items.append(3)
+items.append(5)
+
+#for i in items :
+#	print(i)
+
+names = []
+
+names.append('ikwan')
+names.append('uul')
+names.append('iqbal')
+names.append('doni')
+names.append('irham')
+
+for n in names :
+	print(n + ' ' + ' is cai friend ')
+
---
+++ operators.py
@@ -0,0 +1,26 @@
+number = 1+ 3 * 4 / 6 - 2
+
+#print(number)
+
+modulus = 7 % 3 
+
+#print(modulus)
+
+sqrt = 5 ** 2
+
+#print(sqrt)
+
+cub = 2 ** 3 
+
+#print(cub)
+
+hellos = 'hello ' * 5
+
+#print(hellos)
+
+data1 = [1,3,5]
+data2 = [2,4,6] *2
+
+#print(data1 + data2)
+
+
