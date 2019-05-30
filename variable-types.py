4dd8f64bcf84ada2e890ca64d12ae3d6eea1346a
AndriTinas <andrianustinas.siskom@gmail.com>
2019-05-30 15:21:00 +0700

    variable and data types

---
+++ variable-types.py
@@ -0,0 +1,24 @@
+#isInt = 1
+
+#print(isInt)
+
+#isFloat=7.0
+
+#print(isFloat)
+
+#print(float(6))
+
+#isString='hallo nanas'
+
+#print(isString)
+
+#hallo = 'hallo'
+
+#world ='world'
+
+#print(hallo + ' ' +world)
+
+a , b =  5 ,7 
+
+print(b,a)
+
