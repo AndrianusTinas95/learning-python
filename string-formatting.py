c054a7e6655ba8913ab51de2cd41681e5303b609
AndriTinas <andrianustinas.siskom@gmail.com>
2019-05-30 16:13:06 +0700

    -t

---
+++ string-formatting.py
@@ -0,0 +1,14 @@
+name = 'Cai'
+age =23
+
+#print('%s is %d th ' % (name,age))
+
+data = [1,2,3]
+
+#print(' my data is %s' %data)
+
+dt = ('Tinas' ,'Cai',10)
+
+str = 'Hallo %s %s  you are balance $%d'
+
+print(str % dt)
