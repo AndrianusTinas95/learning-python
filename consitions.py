d197af0099b3ebd43e11ee8fdff9dbcfa3697bea
AndriTinas <andrianustinas.siskom@gmail.com>
2019-05-31 13:25:23 +0700

    condition

---
+++ consitions.py
@@ -0,0 +1,42 @@
+x = 2
+
+#print(x==2)
+
+#print(x==3)
+
+#print(x < 3)
+
+name = 'cai'
+age = 23
+
+
+#if name == 'cai' and age == 23 :
+#	print('your name is cai , and you are also 23 years old')
+
+
+#if name == 'cai' or name == 'coi':
+#	print('your  name is either cai or coi') 
+name ='andri'
+
+#if name in ['cai','coi'] :
+#	print('your name is either cai or coi')
+#else :
+#	print('not cai or coi')
+
+#print(name is 'andri')
+
+x = [1,2,3]
+y = [1,2,3]
+
+
+#print(x  == y )
+#print(x is y)
+#print(x ==  [1,2,3])
+
+#print(not False)
+
+#print((not False)== False)
+
+
+#print(not True == False)
+
